2e980a6dc893f2bbd74cdbe37725c6fbca99fbfc
HelenLviv <helark0@rambler.ru>
2016-07-05 11:30:06 +0000

    fibonachi

---
+++ index.js
@@ -0,0 +1,13 @@
+var Fibonachi=[];
+array.forEach (function (F,n){
+   switch(n <= 30){
+       case 'n == 0':F==0;
+         break;
+       case 'n < 2':F==1;
+         break;
+         default:F,n-1 + F,n-2;
+   }
+    console.log(F);
+var n = Number(prompt("Enter number between 0 and 30"));
+}
+alert result (function (F,n))
